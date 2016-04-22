7cde24f4be057471d5b813c8f9d02d3124c930eb
Nasty Kuznecova <Nasty_Nastiy@mail.ru>
2016-04-22 17:46:30 +0600

    Add output

--- main.c
+++ main.c
@@ -4,5 +4,14 @@
 int main()
 {
 	
+	
+	if (flg != 0) {
+		printf ("no solutions!\n");
+		return EXIT_FAILURE;
+	}
+
+	printf ("x1 = %.2f x2 = %.2f\n", data->x1, data->x2);
+	return EXIT_SUCCESS;
+}
 	return 0;
 }
