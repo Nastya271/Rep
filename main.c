3c9da065a4e5f826e78280efc48940b8d178bbb9
Nasty Kuznecova <Nasty_Nastiy@mail.ru>
2016-04-22 17:35:08 +0600

    input

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include "struct.h"
 
-int main()
+int main(int argc, char **argv)
 {
+
+	if (argc < 4) {
+        printf ("Input a, b, c : (ax^2 + bx + c)\n");
+        return EXIT_FAILURE;
+	}
+
+	int flg = 0;
+    radix *data;
+	data = (radix *) malloc(sizeof(radix));
+	
+    data->a = atoi(argv[1]);
+	data->b = atoi(argv[2]);
+	data->c = atoi(argv[3]);
+	data->x1 = 0;
+	data->x2 = 0;
 	
 	return 0;
 }
---
+++ struct.h
@@ -0,0 +1,11 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+    struct radix {
+        int a;
+        int b;
+        int c;
+        double x1;
+        double x2;
+    };
+#endif
